d37004728939da5c87517b2a1fdfa3efd8167ad7
chyoang30 <chyoang30@gmail.com>
2025-04-12 03:50:18 +0900

    fix: add main

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ def to_speech():
         return {'error': str(e)}, 500
 
 
-
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # 기본값 5000, 환경변수 우선
+    app.run(debug=False, host='0.0.0.0', port=port)
